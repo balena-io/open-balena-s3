9c1e53f1b8dce2a6ddd48f0f62d195284cd2b5f0
Heds Simons <heds@whaleway.net>
2016-10-24 09:45:39 +0100

    Reverts specific `s3-minio` name back to generic Jenkins JOB_NAME.

--- automation/jenkins-build.sh
+++ automation/jenkins-build.sh
@@ -5,14 +5,14 @@ VERSION=$(git rev-parse --short HEAD)
 ESCAPED_BRANCH_NAME=$(echo $sourceBranch | sed 's/[^a-z0-9A-Z_.-]/-/g')
 
 # Try pulling the old build first for caching purposes.
-docker pull resin/resin-s3-minio:${ESCAPED_BRANCH_NAME} || docker pull resin/resin-s3-minio:master || true
+docker pull resin/${JOB_NAME}:${ESCAPED_BRANCH_NAME} || docker pull resin/${JOB_NAME}:master || true
 
-docker build --pull --tag resin/resin-s3-minio:${VERSION} .
+docker build --pull --tag resin/${JOB_NAME}:${VERSION} .
 
-docker tag --force resin/resin-s3-minio:${VERSION} resin/resin-s3-minio:${ESCAPED_BRANCH_NAME}
-docker tag --force resin/resin-s3-minio:${VERSION} resin/resin-s3-minio:latest
+docker tag --force resin/${JOB_NAME}:${VERSION} resin/${JOB_NAME}:${ESCAPED_BRANCH_NAME}
+docker tag --force resin/${JOB_NAME}:${VERSION} resin/${JOB_NAME}:latest
 
 # Push the images
-docker push resin/resin-s3-minio:${VERSION}
-docker push resin/resin-s3-minio:${ESCAPED_BRANCH_NAME}
-docker push resin/resin-s3-minio:latest
+docker push resin/${JOB_NAME}:${VERSION}
+docker push resin/${JOB_NAME}:${ESCAPED_BRANCH_NAME}
+docker push resin/${JOB_NAME}:latest
