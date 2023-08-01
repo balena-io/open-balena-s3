cba50d946f2c868b9a05c4ea8ba5c858747cb654
Otávio Jacobi <otaviojacobi@gmail.com>
2023-08-01 13:48:44 -0300

    Add bucket configuration to create-buckets script
    Change-type: minor

--- scripts/create-buckets.sh
+++ scripts/create-buckets.sh
@@ -6,8 +6,8 @@ if [ -z "${BUCKETS}" ]; then
     BUCKETS="${1:-}"
 fi
 
-# read the list of new buckets we wish to create...
-IFS=';' read -ra NEW_BUCKETS <<< "$BUCKETS"
+# read the list of new buckets and their corresponding base64 encoded config file...
+IFS=';' read -ra BUCKET_ENTRIES <<< "$BUCKETS"
 
 # pull the list of existing buckets...
 # - list the buckets in JSON
@@ -15,11 +15,25 @@ IFS=';' read -ra NEW_BUCKETS <<< "$BUCKETS"
 # - remove the last char, a slash in this case
 EXISTING_BUCKETS=($(mc ls --json localhost/ | jq .key -r | rev | cut -c 2- | rev))
 
-for bucket in "${NEW_BUCKETS[@]}"; do
+for entry in "${BUCKET_ENTRIES[@]}"; do
+    # Split each entry into bucket name and base64 encoded config file (if available)
+    IFS=':' read -r bucket encoded_config <<< "$entry"
+
     echo "Create bucket: $bucket..."
     if [[ ! " ${EXISTING_BUCKETS[@]} " =~ " ${bucket} " ]]; then
         /sbin/mc mb "localhost/${bucket}"
     else
         echo "Bucket already exists: $bucket"
+    fi
+
+    if [ -n "$encoded_config" ]; then
+        echo "$encoded_config" | base64 -d > $bucket.json
+        echo "Applying config:" 
+        cat $bucket.json
+        echo "to bucket: $bucket..."
+
+        /sbin/mc anonymous set-json $bucket.json "localhost/${bucket}"
+    else
+        echo "Config not set for bucket: $bucket"
     fi
 done
